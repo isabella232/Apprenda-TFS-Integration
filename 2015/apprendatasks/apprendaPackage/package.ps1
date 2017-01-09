f6b2fa7fe4cf2ae03a40eda18ad0164f7cc79a90
Chris Dutra <cdutra@apprenda.com>
2017-01-09 12:33:23 -0500

    merged in packaging and added correct inputs.

--- 2015/apprendatasks/apprendaPackage/package.ps1
+++ 2015/apprendatasks/apprendaPackage/package.ps1
@@ -1,25 +1,82 @@
 param(
-  [string] $pathtozip,
-  [string] $alias,
-  [string] $versionPrefix,
-  [string] $stage,
-  [string] $cloudurl,
-  [string] $clouduser,
-  [string] $cloudpw,
-  [string] $clouddevteam
+    [Parameter(Mandatory = $true)][string]  $SolutionPath,
+    [Parameter(Mandatory = $true)][string]  $OutputPath,
+    [Parameter(Mandatory = $false)][string] $Build = "false",
+    [Parameter(Mandatory = $false)][string] $Configuration = "Release",
+    [Parameter(Mandatory = $false)][string] $PrivateUI,
+    [Parameter(Mandatory = $false)][string] $PublicUI,
+    [Parameter(Mandatory = $false)][string] $PrivateRoot,
+    [Parameter(Mandatory = $false)][string] $PublicRoot,
+    [Parameter(Mandatory = $false)][string] $WcfService,
+    [Parameter(Mandatory = $false)][string] $WindowsService,
+    [string] $additionalParams
 )
 
-Import-Module "Microsoft.TeamFoundation.DistributedTask.Task.Common"
-
-try {
-    # Set the working directory.
-    $cwd = Get-VstsInput -Name cwd -Require
-    Assert-VstsPath -LiteralPath $cwd -PathType Container
-    Write-Verbose "Setting working directory to '$cwd'."
-    Set-Location $cwd
-
-    # Output the message to the log.
-    Write-Host (Get-VstsInput -Name msg)
-} finally {
-    Trace-VstsLeavingInvocation $MyInvocation
+try{
+  Write-Verbose "Collecting VSO Variables."
+  $SolutionPath = Get-VstsInput -Name SolutionPath -Require
+  $OutputPath = Get-VstsInput -Name OutputPath -Require
+  $Build = Get-VstsInput -Name Build -Require
+  $Configuration = Get-VstsInput -Name Configuration -Require
+  $PrivateUI = Get-VstsInput -Name PrivateUI -Require
+  $PublicUI = Get-VstsInput -Name PublicUI -Require
+  $PrivateRoot = Get-VstsInput -Name PrivateRoot -Require
+  $PublicRoot = Get-VstsInput -Name PublicRoot -Require
+  $WcfService = Get-VstsInput -Name WcfService -Require
+  $WindowsService = Get-VstsInput -Name WindowsService -Require
 }
+
+$cmd = @"
+& "$PSScriptRoot\acs\acs.exe" NewPackage -Sln "$SolutionPath" -O "$OutputPath" -Config "$Configuration"
+"@
+
+if([System.Convert]::ToBoolean($Build)) {
+    $cmd += " -b"
+}
+
+if(-not [System.String]::IsNullOrEmpty($PrivateUI))
+{
+
+    $cmd += @"
+ -I "$PrivateUI"
+"@
+}
+
+if(-not [System.String]::IsNullOrEmpty($PublicUI))
+{
+    $cmd += @"
+ -PI "$PublicUI"
+"@
+}
+
+if(-not [System.String]::IsNullOrEmpty($PrivateRoot))
+{
+    $cmd += @"
+ -PrivateRoot "$PrivateRoot"
+"@
+}
+
+if(-not [System.String]::IsNullOrEmpty($PublicRoot))
+{
+    $cmd += @"
+ -PublicRoot "$PublicRoot"
+"@
+}
+
+if(-not [System.String]::IsNullOrEmpty($WcfService))
+{
+	$cmd += @"
+ -S "$WcfService"
+"@
+}
+
+if(-not [System.String]::IsNullOrEmpty($WindowsService))
+{
+    $cmd += @"
+ -WS "$WindowsService"
+"@
+}
+
+Write-Host "Executing Command: $cmd"
+
+iex $cmd
